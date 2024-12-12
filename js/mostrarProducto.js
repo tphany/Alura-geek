b904992cf9352ff99e3325b5038a3125f01d10a2
Stephany Castillo M. <stcastillo.uxui@gmail.com>
2024-12-12 01:02:41 -0300

    cambio de color fondo

--- js/mostrarProducto.js
+++ js/mostrarProducto.js
@@ -3,21 +3,3 @@ async function mostrarProductos() {
     const response = await fetch("http://localhost:3000");
     const data = await response.json();
 
-    const productList = document.querySelector('.product-list');
-    productList.innerHTML = ''; // Limpiar contenido existente
-
-    data.forEach(product => {
-      const productCardTemplate = `
-        <li class="product-card">
-          <img src="${product.imagen}" alt="${product.descripcion}" class="product-image">
-          <p class="product-name">${product.nombre}</p><br>
-          <div class="producto__precio__boton">
-            <p class="product-price"><b>$${product.precio}</b></p>
-            <button class="delete-button">
-              <img src="images/delete.png" alt="Eliminar" class="delete-icon">
-            </button>
-          </div>
-        </li>
-      `;
-
-export default mostrarProductos;
